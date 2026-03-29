482d8913cb74a038f01727b5da57e3d99385c479
Henrique Cezar <hcgv1@hotmail.com>
2026-03-29 14:57:08 -0300

    alterando alguns parâmetros na classe Mage

--- src/main/java/br/com/henrique/oopcharacters/model/Mage.java
+++ src/main/java/br/com/henrique/oopcharacters/model/Mage.java
@@ -6,7 +6,6 @@ public class Mage extends Character {
     private String magicType;
 
 
-
     // Construtor
     public Mage(String name, int health, int attack, int defense, int level, int mana, String magicType) {
         super(name, health, attack, defense, level);
@@ -35,31 +34,30 @@ public class Mage extends Character {
     // Métodos sobrescritos
     @Override
     public void attack() {
-        if (mana >= 10) { // custo base de mana para ataque normal
+        if (mana >= 10) {
             mana -= 10;
-            int damage = attack + 10; // ataque mágico básico
-            System.out.println(name + " attacks the enemy with " + magicType + " magic! Damage: " + damage + ". Mana left: " + mana);
+            int damage = attack + 10;
+            System.out.println(name + " ataca o inimigo com magia de " + magicType + damage + ". Mana restante: " + mana);
         } else {
-            System.out.println(name + " tries to attack with magic, but doesn't have enough mana!");
+            System.out.println(name + " tenta atacar com magia mas não tem mana suficiente!");
         }
     }
 
     @Override
     public void defense() {
-        int magicDefense = defense + (mana / 5); // defesa aumentada pelo mana restante
-        System.out.println(name + " defends with a magical barrier! Total defense: " + magicDefense);
+        int magicDefense = defense + (mana / 5);
+        System.out.println(name + " defende com barreira mágica! Defesa Total: " + magicDefense);
     }
 
     public void castSpell(String spellName, int manaCost) {
         if (mana >= manaCost) {
             mana -= manaCost;
-            int damage = attack + (manaCost * 2); // dano proporcional à mana gasta
-            System.out.println(name + " casts " + spellName + " (" + magicType + ")! Damage: " + damage + ". Mana left: " + mana);
+            int damage = attack + (manaCost * 2);
+            System.out.println(name + " lança " + spellName + " (" + magicType + ")! Dano: " + damage + ". Mana restante: " + mana);
         } else {
-            System.out.println(name + " tried to cast " + spellName + " but doesn't have enough mana!");
+            System.out.println(name + " tentou lançar " + spellName + " mas não tem mana suficiente!");
         }
     }
-
 
 
     // Gets e Sets
